de333c5d9866a7495b65fb1567ca43facccddde3
Mahmoud Ahmed <buttererror@gmail.com>
2023-04-13 16:42:42 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {Field, Form, ErrorMessage} from 'vee-validate';
+import * as yup from 'yup';
+
+function isRequired(value) {
+  if (value && value.trim()) {
+    return true;
+  }
+}
+const passwordRules = yup.string().required().min(8);
+
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <Form>
+      <label>
+        password
+      </label>
+      <Field name="password" type="password" :rules="passwordRules" />
+      <ErrorMessage name="password" />
+    </Form>
+
+
+    <!--    <TheWelcome />-->
+  </main>
+</template>
